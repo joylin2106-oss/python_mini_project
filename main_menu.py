cb0bdca9f9ce68c6a28a7a56d25074ec8d929a62
Joylin <joylin2106@gmail.com>
2026-04-03 17:28:20 +0530

    Added function to improve code structure

--- main_menu.py
+++ main_menu.py
@@ -1,55 +1,101 @@
-sum = 0
+# ----------------
+#     MAIN MENU
+#------------------
+import random 
+
+def guess_game():
+    chances = 0 
+    rand_num = random.randint(1,50)
+    while chances<=10:
+         user_value1= int(input("Enter a number:" ))
+         if user_value1 == rand_num :
+            print("Guessed correctly:",user_value1)
+            chances+=1
+            break
+         elif user_value1>=rand_num:
+            print("Higher number")
+         else:
+            print("Lower number") 
+        
+def odd_even(num):
+   
+   if num % 2 ==0:
+     print("EVEN")
+   else: 
+      print("ODD")
+   print(num)
+   return num
+
+def sum_num():
+  sum = 0
+  count = 0 
+  while count<=10:
+   user = int(input("Enter number: "))
+   if user < 0:
+    print("Stop ")
+    
+   else:
+    sum = sum + user
+    print(sum)
+    count+=1  
+    print("Total:", sum)
+  
+ 
+
 while True:
    print("1.games")
    print("2.utilities")
    print("3.Exit")
    choices = int(input("Enter a number: "))
- 
+
+# ----------- GAMES MENU --------------
+  
    if choices== 1:
-        print("game")
+        print("-----game-----")
         print("Welcome to Games section")
-        
+
+#------------ GUESSING GAME -----------     
+ 
         while True:
-            print("1.Guessing game ")
+            print("1.Guessing game")
             print("2.back ")
             user_value = int(input("Enter a number: "))
             if user_value == 1:
-             print("Guessing game")
+             print("----Guessing game----")
+             guess_game()
             else: 
              print("Go Back")  
              break   
+
+# ---------- UTILITIES MENU ------------
+   
    elif choices==2:
-     print("utilities")
+     print("-----utilities-----")
      print("Welcome to utilities section")
      while True:
+
+# --------- ODD/EVEN -------------
+       
         print("1.Even/Odd")
         print("2.Sum of number")
         print("3.Go back")
-        user_inpt2 = int(input("Enter a Number: "))
-        if user_inpt2 == 1:
-           num = int(input("Enter any desire number: "))
-           print("")
-           if num % 2 ==0:
-              print("EVEN")
-           else: 
-              print("ODD")  
-     
-        elif user_inpt2 == 2:
-          sum = 0
-          while True:
-             user = int(input("Enter number: "))
-             if user < 0:
-               print("Stop ")
-               break
-             else:
-              sum = sum + user
-              print(sum)
-          print("Total:", sum)     
-                  
         
-        elif user_inpt2 == 3:
+        user_inpt1 = int(input("Enter a Number: "))
+        
+        if user_inpt1 == 1:
+           print("-----EVEN/ODD-----")
+           num = int(input("Enter any desire number: "))
+           odd_even(num)
+
+# ---------- SUM - NUMBER --------------
+        elif user_inpt1 == 2:
+          print("-----sum of number-----")
+          sum_num()     
+        elif user_inpt1 == 3:
            print("Go Back")
            break
+        else:
+           print("Invalid")
             
         
    elif choices == 3:
