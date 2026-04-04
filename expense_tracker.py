9a0f6ff50972b371b1c6d43e73736123cbe5d174
Joylin <joylin2106@gmail.com>
2026-04-04 17:33:32 +0530

    Added loop for multiple expense input

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,39 +1,22 @@
-date = input("Enter the date: ")
-item = input("Enter the item: ")
-amount = int(input("Enter the amount: "))
-
-
-food_exp = {
-    "date":date,
-    "item" : item,
-    "amount" :amount
-}
-
-print(food_exp["date"])
-print(food_exp["item"])
-print(food_exp["amount"])
-
-
-date = input("Enter the date: ")
-item = input("Enter the item: ")
-amount = int(input("Enter the amount: "))
-
-food_exp1={
-    "date":date,
-    "item" : item,
-    "amount" :amount
-}
-
-print(food_exp1["date"])
-print(food_exp1["item"])
-print(food_exp1["amount"])
 
 exp_store =[]
-exp_store.append(food_exp)
-exp_store.append(food_exp1)
+expenses = 0 
+n = int(input("Enter number: "))
+for i in range (1,n+1):
+    date = input("Enter the date: ")
+    item = input("Enter the item: ")
+    amount = int(input("Enter the amount: "))
 
+
+    food_exp = {
+    "date":date,
+    "item" : item,
+    "amount" :amount
+    }
+    exp_store.append(food_exp)
 print(exp_store)
-
+for exp in exp_store:
+    print(exp["date"],exp["item"],exp["amount"])
 
 
 
